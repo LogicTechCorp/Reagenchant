7fb717085058c5c6f5d4f65cffc38c0ba14c0a73
LogicTechCorp <logictechcorp@gmail.com>
2019-06-24 01:47:40 -0700

    Fixed armor with unbreaking being able to break Changed curse of binding so that it is removed from broken armor

--- src/main/java/logictechcorp/reagenchant/handler/UnbreakingHandler.java
+++ src/main/java/logictechcorp/reagenchant/handler/UnbreakingHandler.java
@@ -19,20 +19,29 @@ package logictechcorp.reagenchant.handler;
 
 import logictechcorp.reagenchant.Reagenchant;
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Enchantments;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.CombatRules;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.IShearable;
+import net.minecraftforge.common.ISpecialArmor;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.event.entity.player.PlayerEvent.HarvestCheck;
@@ -171,6 +180,7 @@ public class UnbreakingHandler
     @SubscribeEvent
     public static void onLivingHurt(LivingHurtEvent event)
     {
+        Entity attacked = event.getEntity();
         DamageSource source = event.getSource();
         Entity attacker = source.getTrueSource();
 
@@ -184,18 +194,11 @@ public class UnbreakingHandler
                 event.setAmount(1.0F);
             }
         }
-    }
-
-    private static boolean isItemBroken(ItemStack stack)
-    {
-        int usesRemaining = (stack.getMaxDamage() - stack.getItemDamage());
-
-        if(usesRemaining == 0)
+        if(attacked instanceof EntityPlayer)
         {
-            stack.setItemDamage(stack.getMaxDamage() - 1);
+            UnbreakingHandler.damagePlayer((EntityPlayer) attacked, source, event.getAmount());
+            event.setAmount(0.0F);
         }
-
-        return !stack.isEmpty() && !EnchantmentHelper.hasBindingCurse(stack) && EnchantmentHelper.getEnchantmentLevel(Enchantments.UNBREAKING, stack) > 0 && usesRemaining <= 1;
     }
 
     public static void addBrokenPropertyToItems()
@@ -204,5 +207,112 @@ public class UnbreakingHandler
         {
             item.addPropertyOverride(BROKEN_PROPERTY_KEY, BROKEN_PROPERTY);
         }
+    }
+
+    private static void damagePlayer(EntityPlayer player, DamageSource source, float amount)
+    {
+        NonNullList<ItemStack> armorStacks = player.inventory.armorInventory;
+        NonNullList<ItemStack> brokenArmorStacks = NonNullList.withSize(4, ItemStack.EMPTY);
+
+        for(int i = 0; i < armorStacks.size(); i++)
+        {
+            if(UnbreakingHandler.isItemBroken(armorStacks.get(i)))
+            {
+                brokenArmorStacks.set(i, armorStacks.set(i, ItemStack.EMPTY));
+            }
+        }
+
+        float damage = ISpecialArmor.ArmorProperties.applyArmor(player, armorStacks, source, amount);
+
+        if(!source.isDamageAbsolute())
+        {
+            if(player.isPotionActive(MobEffects.RESISTANCE) && source != DamageSource.OUT_OF_WORLD)
+            {
+                int potionAmplifier = (player.getActivePotionEffect(MobEffects.RESISTANCE).getAmplifier() + 1) * 5;
+                damage = damage * (25.0F - potionAmplifier) / 25.0F;
+            }
+
+            if(damage <= 0.0F)
+            {
+                damage = 0.0F;
+            }
+            else
+            {
+                int enchantmentModifierDamage = EnchantmentHelper.getEnchantmentModifierDamage(armorStacks, source);
+
+                if(enchantmentModifierDamage > 0)
+                {
+                    damage = CombatRules.getDamageAfterMagicAbsorb(damage, enchantmentModifierDamage);
+                }
+            }
+        }
+
+        float totalDamage = damage;
+        damage = Math.max(damage - player.getAbsorptionAmount(), 0.0F);
+        player.setAbsorptionAmount(player.getAbsorptionAmount() - (totalDamage - damage));
+        damage = ForgeHooks.onLivingDamage(player, source, damage);
+
+        if(damage != 0.0F)
+        {
+            float playerHealth = player.getHealth();
+            player.addExhaustion(source.getHungerDamage());
+            player.setHealth(playerHealth - damage);
+            player.getCombatTracker().trackDamage(source, playerHealth, damage);
+
+            if(damage < 3.4028235E37F)
+            {
+                player.addStat(StatList.DAMAGE_TAKEN, Math.round(damage * 10.0F));
+            }
+        }
+
+        for(int i = 0; i < armorStacks.size(); i++)
+        {
+            ItemStack armorStack = armorStacks.get(i);
+
+            if(!armorStack.isEmpty() && !UnbreakingHandler.isItemBroken(armorStack) && armorStack.getMaxDamage() == armorStack.getItemDamage())
+            {
+                armorStacks.set(i, ItemStack.EMPTY);
+            }
+        }
+
+        for(int i = 0; i < brokenArmorStacks.size(); i++)
+        {
+            ItemStack armorStack = brokenArmorStacks.set(i, ItemStack.EMPTY);
+
+            if(!armorStack.isEmpty())
+            {
+                armorStacks.set(i, armorStack);
+            }
+        }
+    }
+
+    private static boolean isItemBroken(ItemStack stack)
+    {
+        int usesRemaining = (stack.getMaxDamage() - stack.getItemDamage());
+        boolean canBeBroken = !stack.isEmpty() && EnchantmentHelper.getEnchantmentLevel(Enchantments.UNBREAKING, stack) > 0;
+        boolean isBroken = canBeBroken && usesRemaining <= 1;
+
+        if(usesRemaining == 0 && canBeBroken)
+        {
+            stack.setItemDamage(stack.getMaxDamage() - 1);
+        }
+
+        if(isBroken && EnchantmentHelper.hasBindingCurse(stack))
+        {
+            NBTTagList enchantmentList = stack.getTagCompound().getTagList("ench", 10);
+
+            for(int i = 0; i < enchantmentList.tagCount(); i++)
+            {
+                NBTTagCompound compound = enchantmentList.getCompoundTagAt(i);
+
+                if(compound.getShort("id") == Enchantment.getEnchantmentID(Enchantments.BINDING_CURSE))
+                {
+                    enchantmentList.removeTag(i);
+                    break;
+                }
+            }
+        }
+
+        return isBroken;
     }
 }
